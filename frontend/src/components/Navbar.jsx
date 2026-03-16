19786947e2283972af893f554b84596ecf5d8961
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-03-17 00:43:56 +0530

    feat: add lucide-react dependency and update Navbar component with Clock icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import { navbarStyles } from '../assets/dummyStyles'
+import React, { useState } from "react";
+import { navbarStyles } from "../assets/dummyStyles";
+import { Clock } from "lucide-react";
+import { useLocation, useNavigate, Link } from "react-router-dom";
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [active, setActive] = useState(location.pathname || "/");
+
+  const handleNavClick = (href) => {
+    setActive(href);
+    setOpen(false);
+  };
   return (
     <header className={navbarStyles.header}>
-        <nav className={navbarStyles.nav} role='navigations'>
-            <div className={navbarStyles.container}>
-                {/* BRAND LOGO */}
-                <div className={navbarStyles.brandContainer}>
-                    <div className={navbarStyles.logoContainer}>
-
-                    </div>
-                </div>
+      <nav className={navbarStyles.nav} role="navigation">
+        <div className={navbarStyles.container}>
+          {/* BRAND LOGO */}
+          <div className={navbarStyles.brandContainer}>
+            <div className={navbarStyles.logoContainer}>
+              <Clock className={navbarStyles.logoIcon} />
             </div>
-        </nav>
-
+            <Link
+              to="/"
+              onClick={() => handleNavClick("/")}
+              className={navbarStyles.logoLink}
+            >
+              <span
+                style={navbarStyles.logoTextStyle}
+                className={navbarStyles.logoText}
+              >
+                ChoronoElite
+              </span>
+            </Link>
+          </div>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
