3480d69702567c2528197c50274fd3604e5af0cc
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-03-18 01:09:02 +0530

    Add brand navigation and back button to BrandPage component
    - Implemented brand-specific watch data retrieval using useParams.
    - Added a back button with navigation functionality to return to the previous page.
    - Styled the header and back button for improved user experience.

--- frontend/src/components/BrandPage.jsx
+++ frontend/src/components/BrandPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { brandPageStyles } from "../assets/dummyStyles";
+import { useNavigate, useParams } from "react-router-dom";
+import watchesData from "../assets/CategoriesHomedata";
+import { useCart } from "../CartContext";
+import { ArrowLeft } from "lucide-react";
 
 const BrandPage = () => {
+  const { brandName } = useParams();
+  const navigate = useNavigate();
+  const bramdWatches = watchesData[brandName?.toLocaleLowerCase()] || [];
+  const { addItem } = useCart();
   return (
-    <div>BrandPage</div>
-  )
-}
+    <div className={brandPageStyles.mainContainer}>
+      <div className="max-w-7xl mx-auto relative">
+        <div className={brandPageStyles.headerContainer}>
+          <div className={brandPageStyles.backButtonContainer}>
+            <button
+              className={brandPageStyles.backButton}
+              onClick={() => navigate(-1)}
+            >
+              <div className={brandPageStyles.backIconContainer}>
+                <ArrowLeft size={20} />
+              </div>
+              <span className={brandPageStyles.backText}>Back</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BrandPage
+export default BrandPage;
