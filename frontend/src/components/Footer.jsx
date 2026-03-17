951d7f904ba138ebeba1e7d5e5ffd063cd361d2d
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-03-17 23:58:47 +0530

    feat: enhance Footer component with newsletter subscription form and social media icons

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { footerStyles } from '../assets/dummyStyles';
+import React from "react";
+import { footerStyles } from "../assets/dummyStyles";
+import { Clock, Facebook, Instagram, Twitter } from "lucide-react";
 
 const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
-        <div className={footerStyles.topBorder}>
-        </div>
-           {/* Pattern overlay */}
+      <div className={footerStyles.topBorder}></div>
+      {/* Pattern overlay */}
       <div className={footerStyles.patternOverlay}>
         <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
           <defs>
@@ -71,21 +71,55 @@ const Footer = () => {
       </div>
       <div className={footerStyles.mainContainer}>
         <div className={footerStyles.newsletterSection}>
-            <div className={footerStyles.newsletterContent}>
-                <h3 className={footerStyles.newsletterTitle}>
-                    Timeless Elegance, Delivered
-                </h3>
-                <p className={footerStyles.newsletterText}>
-                    Subscribe to our newsletter for exclusive offers, new Collection announcements, and styling tips.
-                </p>
+          <div className={footerStyles.newsletterContent}>
+            <h3 className={footerStyles.newsletterTitle}>
+              Timeless Elegance, Delivered
+            </h3>
+            <p className={footerStyles.newsletterText}>
+              Subscribe to our newsletter for exclusive offers, new Collection
+              announcements, and styling tips.
+            </p>
 
+            <div className={footerStyles.formContainer}>
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className={footerStyles.emailInput}
+              />
+              <button className={footerStyles.subscribeButton}>
+                Subscribe
+              </button>
             </div>
+          </div>
+        </div>
 
+        {/* Main footer */}
+        <div className={footerStyles.mainGrid}>
+          <div className={footerStyles.brandSection}>
+            <div className={footerStyles.brandContainer}>
+              <div className={footerStyles.brandIconContainer}>
+                <div className={footerStyles.brandIconPing}></div>
+                <Clock className={footerStyles.brandIcon} />
+              </div>
+              <span className={footerStyles.brandName}>ChronoElite</span>
+            </div>
+            <p className={footerStyles.brandDescription}>
+              Crafting timeless pieces for the discwening individual. Where
+              precision meets elegance in every detail.
+            </p>
+
+            <div className={footerStyles.socialIconsContainer}>
+              {[Facebook, Instagram, Twitter].map((Icon, index) => (
+                <a href="#" key={index} className={footerStyles.socialIcon}>
+                  <Icon className={footerStyles.socialIconInner} />
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
