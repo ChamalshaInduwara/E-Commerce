a19c08201c7c4d30ae8c74ce1a43ce52cbe5527f
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-03-18 16:34:34 +0530

    feat: add font import for improved typography in LoginPage component

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -190,7 +190,10 @@ const LoginPage = () => {
             </div>
             </div>
         </div>
-        
+        {/* Add font import */}
+      <style>
+        {`@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');`}
+      </style>
     </div>
   )
 }
