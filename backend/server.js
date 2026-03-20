2602629ee2a811a25d5b2e68694d778f0be68588
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-03-21 00:30:49 +0530

    feat: add watch model, controller, and routes for managing watches

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import path from "path"
 import userRouter from "./routes/userRoute.js";
+import watchRouter from "./routes/watchRoute.js";
 
 const app = express();
 const port = 4000;
@@ -17,6 +19,8 @@ connectDB();
 
 // ROUTES
 app.use("/api/auth", userRouter);
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/api/watches", watchRouter);
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
