99c017fd6ba8474ab79b7e79552ddb9771eeea7c
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-03-21 20:17:54 +0530

    feat: add order routes for creating and confirming orders

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
 
 import authMiddleware from "../middleware/auth.js";
+import { confirmPayment, createOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
+orderRouter.post("/", authMiddleware,createOrder)
+orderRouter.get("/confirm", confirmPayment);
