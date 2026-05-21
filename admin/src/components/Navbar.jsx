e431d3c3b58ee763fe383a1e3d1dcf41b08384c0
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-05-21 15:48:09 +0530

    Clean up Navbar component structure and improve readability

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import { navbarStyles } from '../assets/dummyStyles';
-import { Clock } from 'lucide-react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { navbarStyles } from "../assets/dummyStyles";
+import { Clock } from "lucide-react";
 
 const Navbar = () => {
   return (
     <header className={navbarStyles.header}>
-        <div className={navbarStyles.container}>
-            <div className={navbarStyles.mainBar}>
-                <div className={navbarStyles.brandContainer}>
-                    <div className={navbarStyles.brandLogo}>
-                        <Clock className={navbarStyles.brandIcon} />
-                    </div>
-                    
-                </div>
+      <div className={navbarStyles.container}>
+        <div className={navbarStyles.mainBar}>
+          <div className={navbarStyles.brandContainer}>
+            <div className={navbarStyles.brandLogo}>
+              <Clock className={navbarStyles.brandIcon} />
             </div>
+            <NavLink
+              to="/"
+              className={navbarStyles.brandText}
+              style={{
+                fontFamily:
+                  'Poppins, ui-sans-serif, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial',
+              }}
+            >
+              ChronoElite
+            </NavLink>
+          </div>
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
